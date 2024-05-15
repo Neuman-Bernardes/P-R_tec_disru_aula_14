1c1a2df925d63a000918d6bfc892e8afc7fe8584
Neuman <neumanbernardes@gmail.com>
2024-05-15 20:43:20 -0300

    first commit

---
+++ aplicacao/backend/src/main/java/com/projeto/integrado/repository/StatusTarefaRepository.java
@@ -0,0 +1,9 @@
+package com.projeto.integrado.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.projeto.integrado.entity.StatusTarefa;
+
+public interface StatusTarefaRepository extends JpaRepository<StatusTarefa, Integer> {
+	Optional<StatusTarefa> findByDescricao(String descricao);
+}
---
+++ aplicacao/backend/src/main/java/com/projeto/integrado/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.projeto.integrado.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.projeto.integrado.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+	Optional<User> findByEmail(String email);
+}
